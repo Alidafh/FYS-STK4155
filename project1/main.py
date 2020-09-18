150236f9055258e745664fc765b91a2b71aa0c11
alidafh <alidafh@gmail.com>
2020-09-18 15:50:50 +0200

    train,test splitting

--- project1/main.py
+++ project1/main.py
@@ -3,9 +3,35 @@ import numpy as np
 import pandas as pd
 from imageio import imread
 import general_functions as func
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
-x, y, z = func.GenerateData(4, 0, 1, 0.01, "debug")
+x, y, z = func.GenerateData(100, 0, 1, 0.01, "debug")
 
 features = func.PolyDesignMatrix(x,y,degree=2)
-df = pd.DataFrame(features)
-print(df)
+
+x = x.ravel()
+y = y.ravel()
+z = z.ravel()
+
+x_train, x_test, y_train, y_test, z_train, z_test = train_test_split(x, y, z, test_size=0.33)
+
+print(np.shape(x))
+print(np.shape(x_train), np.shape(x_test))
+
+
+
+
+#df = pd.DataFrame(features)
+
+
+"""
+terrain1 = imread("datafiles/SRTM_data_Norway_1.tif")
+
+plt.figure()
+plt.title("Terrain over Norway 1")
+plt.imshow(terrain1, cmap="gray")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+"""
