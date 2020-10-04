c7ca4aa141e128153419010474b50cb63192dc1e
alidafh <alidafh@gmail.com>
2020-10-04 19:09:28 +0200

    cleanup docstrings

--- project1/functions.py
+++ project1/functions.py
@@ -17,27 +17,36 @@ def FrankeFunction(x,y):
         x: numpy array or scalar
         y: numpy array or scalar
     --------------------------------
+    Returns:
+        z: numpy array or scalar representing the function value
+    --------------------------------
     TO DO: FINISHED
     """
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
     term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-    return term1 + term2 + term3 + term4
+    z = term1 + term2 + term3 + term4
+    return z
 
 def GenerateData(nData, noise_str=0, seed=""):
     """
-    Generates data for the Franke function with x,y:[start,stop]
+    Generates three numpy arrays x, y, z of size (nData, 1).
+    The x and y arrays are randomly distributed numbers between 0 and 1.
+    The z array is created using the Franke Function with x and y, and if a
+    noise_str is specified, random normally distributed noise with strength
+    noise_str is added to the z-array.
     --------------------------------
     Input
         nData: number of datapoints
         noise_str: the strength of the noise, default is zero
         seed: if set to "debug" random numbers are the same for each turn
     --------------------------------
-    return
-        x: random numpy array [0,1]
-        y: random numpy array [0,1]
-        z: data from the franke function at given x and y values
+    Returns
+        x: numpy array of shape (n,1) with random numbers between 0 and 1
+        y: numpy arary of shape (n,1) with random numbers between 0 and 1
+        z: numpy array of shape (n,1) with Franke function values f(x,y)
+    --------------------------------
     TO DO: FINISHED
     """
 
@@ -57,30 +66,32 @@ def GenerateData(nData, noise_str=0, seed=""):
 
     return x, y, z
 
-def PolyDesignMatrix(x, y, degree):
+def PolyDesignMatrix(x, y, d):
     """
-    Generates a design matrix of size (n,p) using a polynomial of chosen degree
+    Generates a design matrix of size (n,p) with monomials up to degree d as
+    the columns. As an example if d=2 the columns of the design matrixs will be
+    [1  x  y  x**2  y**2  xy].
     --------------------------------
     Input
-        x: numpy array with shape (n,n)
-        y: numpy array with shape (n,n)
-        degree: the degree of the polynomial
+        x: numpy array with shape (n,) or (n,1)
+        y: numpy array with shape (n,) or (n,1)
+        d: the degree of the polynomial (scalar)
     --------------------------------
     Returns
-        X: Design matrix
-
+        X: Design matrix of shape (n, p)
+    --------------------------------
     TO DO: FINISHED
     """
-    if len(x.shape) > 1:    # Easier to use arrays with shape (n, 1) where n = m**2
+    if len(x.shape) > 1:
         x = x.ravel()
         y = y.ravel()
 
     n = len(x)
-    p = int(((degree+2)*(degree+1))/2)  # number of terms in beta
-    X = np.ones((n, p))     # Matrix of size R^nxp where all entries are zero
+    p = int(((d+2)*(d+1))/2)  # number of terms in beta
+    X = np.ones((n, p))       # Matrix of size (n,p) where all entries are zero
 
     # fill colums of X with the polynomials [1  x  y  x**2  y**2  xy ...]
-    for i in range(1, degree+1):
+    for i in range(1, d+1):
         j = int(((i)*(i+1))/2)
         for k in range(i+1):
             X[:,j+k] = x**(i-k)*y**(k)
@@ -88,13 +99,17 @@ def PolyDesignMatrix(x, y, degree):
 
 def scale_X(train, test):
     """
-    Scales the training and test data using sklearn StandardScaler
+    Scales the training and test data using sklearn's StandardScaler.
     --------------------------------
     Input
         train: The training set
-        test: The test set
+        test:  The test set
     --------------------------------
-    returns train_scl and test_scl
+    Returns
+        train_scl: The scaled training set
+        test_scl:  The scaled test set
+    --------------------------------
+    TO DO: FINISHED
     """
     scaler = StandardScaler()
     scaler.fit(train[:,1:])
@@ -117,13 +132,12 @@ def metrics(z_true, z_pred, test=False):
         z_approx: The approximation found using regression
         test: If you want to test the calculations (default is False)
     --------------------------------
-    return: R2, MSE, var, bias
+    Returns
+        R2, MSE, var, bias
+    --------------------------------
     TODO: When using this with bootstrap, the calculated R2 score is waay off
           and when using it with kFold the bias and the MSE are identical
-          #R2 = 1-((z_true-z_pred).T@(z_true-z_pred))/((z_true-np.mean(z_true)).T@(z_true-np.mean(z_true)))
     """
-
-    #if np.shape(z_pred) != (len(z_true), 1)
     n = len(z_true)
     R2= 1 - ((np.sum((z_true - z_pred)**2))/(np.sum((z_true - np.mean(z_true))**2)))
     MSE = np.mean(np.mean((z_true - z_pred)**2, axis=1, keepdims=True))
@@ -134,7 +148,6 @@ def metrics(z_true, z_pred, test=False):
 
     if test == True:
         if np.shape(z_pred) == (len(z_pred), 1):
-            #print("Testing with sklearn:")
             r2_sklearn = r2_score(z_true, z_pred)
             mse_sklearn = mean_squared_error(z_true, z_pred)
 
@@ -155,7 +168,6 @@ def metrics(z_true, z_pred, test=False):
                 print("-------------\n")
 
     return R2, MSE, var, bias
-    #return r2_sklearn, mse_sklearn, var, bias
 
 def OLS_SVD(z, X, var = False):
     """
@@ -164,11 +176,15 @@ def OLS_SVD(z, X, var = False):
     variance of the fitted parameters
     --------------------------------
     Input
-        z: response variable
-        X: Design matrix
-        var: To calculate the variance set this to True (default is False)
+        z: response variable of shape (n,1) or (n,)
+        X: Design matrix of shape (n,p)
+        var: Bool. Set this to True to calculate the variance (default is False)
     --------------------------------
-    TODO: Finished
+    Returns
+        beta: The estimated OLS regression parameters shape (p,1)
+        (var_beta: The variance of the parameters are returned if var=True)
+    --------------------------------
+    TO DO: FINISHED
     """
 
     U, D, Vt = np.linalg.svd(X)
@@ -197,40 +213,43 @@ def OLS_SVD(z, X, var = False):
 
 def OLS(z, X, var = False):
     """
-    Preforming ordinary least squares fit to find the regression parameters
-    beta. If prompted it calculates the variance of the fitted parameters.
-    An error message will be printed if the design matrix has high dimentionality,
-    p > n, but the parameters are still calculated. As this would give a negative
-    variance, a temporary workaround is to take the absolute value of sigma^2.
+    Preforming ordinary least squares fit to find the regression parameters.
+    If prompted it also calculates the variance of the fitted parameters.
+    An error message will be printed if the design matrix has high
+    dimentionality, p > n, but the parameters are still calculated.
+    As this would give a negative variance, a temporary workaround is to take
+    the absolute value of sigma^2.
     --------------------------------
     Input
         z: response variable
         X: Design matrix
         var: To calculate the variance set this to True (default is False)
     --------------------------------
+    Returns
+    - var=False
+        beta: The estimated OLS regression parameters, shape (p,1)
+        (var_beta: The variance of the parameters (p,1), returned if var=True)
+    --------------------------------
     TODO: Find out if the absoultevalue thing in variance calculations is legit.
     """
 
     #beta = np.linalg.pinv(X) @ z
-    beta = np.linalg.pinv(X.T @ X) @ X.T @ z
+    beta = np.linalg.pinv(X.T @ X) @ X.T @ z # To be consistent with Ridge
 
     if len(z) < len(beta):
         print("ERROR: n = {} < p = {}". format(len(z), len(beta)))
         print("Remember that OLS is not well defined for p > n!\n")
 
     if var == True:
-        # Calculate the variance of the regression parameter
         z_pred = X @ beta
-
         sigma2 = np.sum((z - z_pred)**2)/(len(z)-len(beta)-1)
         #sigma2 = np.sum((z - z_pred)**2)/(len(z)-len(beta))
-        if sigma2 <= 0:
-            sigma2 = np.abs(sigma2)
-
+        if sigma2 <= 0: sigma2 = np.abs(sigma2)
         var_beta = sigma2 * SVDinv(X.T @ X).diagonal()
-        return beta, var_beta
-    return beta
 
+        return beta, var_beta.reshape(len(var_beta),1)
+
+    return beta
 
 def Ridge(z, X, lamb, var=False):
     """
@@ -243,6 +262,10 @@ def Ridge(z, X, lamb, var=False):
         lamb: penalty parameter
         var: to calculate the variance set this to True (default is False)
     --------------------------------
+    Returns
+        beta: The estimated Ridge regression parameters with shape (p,1)
+        (var_beta: The variance of the parameters (p,1), returned if var=True)
+        --------------------------------
     TODO: finish variance
     """
     I = np.eye(X.shape[1])
@@ -255,11 +278,9 @@ def Ridge(z, X, lamb, var=False):
     return beta
 
 
-
 if __name__ == '__main__':
     x, y, z = GenerateData(10, 0.01, "debug")
     X = PolyDesignMatrix(x, y, 2)
-
     # Test if the returned beta's are the same for small lambdas:
     beta_ridge, var_ridge = Ridge(z, X, 0.0000001, True)
     beta_ols, var_ols = OLS(z,X, True)
