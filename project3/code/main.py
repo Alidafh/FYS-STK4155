2240a0cf3fbe9d5f5efd51f9794bb7676e15996f
alidafh <alidafh@gmail.com>
2020-12-02 10:14:50 +0100

    testing

--- project3/code/main.py
+++ project3/code/main.py
@@ -9,7 +9,6 @@ deprecation._PRINT_DEPRECATION_WARNINGS = False
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,17 +16,20 @@ import configuration as conf
 from CNN import create_model, train_model
 from generate import load_data
 
+
+
 def plot_training_data(label_names=None, slice=0):
     plt.figure(figsize=(10,10))
     for i in range(25):
         plt.subplot(5, 5,i+1)
         plt.xticks([])
         plt.yticks([])
-        plt.imshow(X_train[i][:,:,slice], cmap="inferno")
+        plt.imshow(X_train[i][:,:,slice], cmap="gray")
+        n = y_train[i].argmax()
         if label_names:
-            plt.xlabel("{:}".format(label_names[int(y_train[i])]))
+            plt.xlabel("{:}".format(label_names[n]))
         else:
-            plt.xlabel("{:}".format(y_train[i]))
+            plt.xlabel("{:}".format(n))
     plt.show()
 
 def plot_test_results(label_names=None, slice=0):
@@ -46,54 +48,55 @@ def plot_test_results(label_names=None, slice=0):
             plt.xlabel("{:} (true={:})".format(n,m))
     plt.show()
 
-
 def plot_history(history):
+    plt.figure(figsize=(10,4))
+    plt.subplot(1,2,1)
     plt.plot(history.history['accuracy'], label='accuracy')
     plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
-    #plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+
+    plt.subplot(1,2,2)
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label = 'val_loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
     plt.legend(loc='lower right')
     plt.show()
+    plt.show()
 
-"""
-# MNIST data
+def mnist():
+    # MNIST data
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+    X_train = X_train.reshape(60000,28,28,1)
+    X_test = X_test.reshape(10000,28,28,1)
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+    plot_training_data()
+    return (X_train, y_train), (X_test, y_test)
 
-(X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
-
-X_train = X_train.reshape(60000,28,28,1)
-X_test = X_test.reshape(10000,28,28,1)
-
-X_train, X_test = X_train / 255.0, X_test / 255.0
-plot_training_data()
-
-"""
 
 # GCE data
 maps, labels, stats = load_data(file=conf.data_file, slice=conf.slice)
+
 label_names = ["Clean", "DM"]
+labels = to_categorical(labels)
 
-X_train, X_test, y_train, y_test = train_test_split(maps, labels, test_size=0.2, random_state=42)
+maps = maps/maps.max()
 
-mean = np.mean(X_train)
-std = np.std(X_train)
+X_train, X_test, y_train, y_test = train_test_split(maps, labels, train_size=0.8)
 
-X_train = (X_train - mean)/std
-X_test = (X_test - mean)/std
-
-plot_training_data(label_names=label_names, slice=5)
-
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
+#plot_training_data(label_names=label_names, slice=9)
 
 model = create_model()
-
-history = train_model(X_train, y_train, X_test, y_test, model, verbose=1)
+history = train_model(X_train, y_train, X_test, y_test, model, verbose=1, save_as="GCE")
 
 loss, acc = model.evaluate(X_test, y_test, verbose=2)
 print(65*"_", "accuracy: {:5.2f}%".format(100 * acc), 65*"_", sep="\n")
 
 y_pred = model.predict(X_test)
 
-plot_test_results(label_names=label_names, slice=5)
+#plot_test_results(label_names=label_names)
+#plot_test_results()
 plot_history(history)
