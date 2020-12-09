53391f84e713d1d4598c11295d73864eaacd1f3a
alidafh <alidafh@gmail.com>
2020-12-09 19:26:22 +0100

    added parser

--- project3/code/CNN.py
+++ project3/code/CNN.py
@@ -7,15 +7,12 @@ deprecation._PRINT_DEPRECATION_WARNINGS = False
 from pathlib import Path
 import tensorflow as tf
 import numpy as np
-import configuration as conf
-
-
+import matplotlib.pyplot as plt
 
 def create_model():
     """
     Creates a model using the configurations in the configuration file
     """
-
     model = tf.keras.Sequential()
 
     model.add(tf.keras.layers.Conv2D(conf.n_filters,
@@ -61,14 +58,46 @@ def train_model(X_train, y_train, X_val, y_val, model, save_as=None, verbose=0):
     """
     Train the model using the configurations in the configuration file
     """
-
+    from tensorflow.keras.callbacks import CSVLogger
     model.compile(optimizer=conf.opt, loss=conf.loss, metrics=conf.metrics)
 
-    if verbose == 1:
+    if verbose != 0:
         print(model.summary())
-        loss, acc = model.evaluate(X_val, y_val, verbose=0)
-        print("Untrained, accuracy: {:5.2f}%".format(100*acc),65*"_", sep="\n" )
 
+        if conf.type == "classification":
+            loss, acc = model.evaluate(X_val, y_val, verbose=0)
+            print("Untrained, accuracy: {:5.2f}%".format(100*acc),65*"_", sep="\n" )
+
+        if conf.type == "regression":
+            loss, r2 = model.evaluate(X_val, y_val, verbose=0)
+            print(65*"_", "Untrained, r2: {:5.2f}%".format(100*r2), 65*"_", sep="\n")
+
+
+    csv_logger = None
+
+    if save_as is not None:
+        Path(conf.model_dir).mkdir(parents=True, exist_ok=True)
+        csv_logger = CSVLogger(conf.model_dir+save_as+'_training.log', separator=',', append=False)
+
+    history = model.fit(X_train, y_train, batch_size = conf.batch_size,
+                                          validation_data=(X_val, y_val),
+                                          epochs=conf.epochs,
+                                          callbacks = [csv_logger, conf.early_stop])
+
+    if save_as is not None:
+        model.save(conf.model_dir+"{:}".format(save_as))
+
+    return history
+
+
+def continue_training(X_train, y_train, X_val, y_val, model_name, save_as=None, verbose=0):
+    """ continue training on existing model NOT DONE """
+
+    model = tf.keras.models.load_model(conf.model_dir+"{:}".format(model_name))
+
+    np.testing.assert_allclose(
+        model.predict(test_input), reconstructed_model.predict(test_input)
+        )
 
     history = model.fit(X_train, y_train, batch_size = conf.batch_size,
                                           validation_data=(X_val, y_val),
@@ -82,15 +111,85 @@ def train_model(X_train, y_train, X_val, y_val, model, save_as=None, verbose=0):
 
     return history
 
+def arguments():
+    """
+    Choose which type of network you want to run using command line flags.
 
-def get_model(model_name):
-    """ retrieve model from file """
+    """
+    import argparse
+    import sys
 
-    model = tf.keras.models.load_model(conf.model_dir+"{:}".format(model_name))
+    description =  """Train the CNN"""
+    frm =argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(description=description, formatter_class=frm)
 
-    return model
+    required = parser.add_argument_group('required arguments')
+    required.add_argument('-r', action='store_true', help="Regression")
+    required.add_argument('-c', action='store_true', help="Classification")
+
+    parser.add_argument('-n', type=str, metavar='name', action='store', default=None,
+                    help='What name to store the model as')
+
+    parser.add_argument('-e', action='store_true', help='continue training on existing model, NOT DONE')
+
+    args = parser.parse_args()
+
+    r, c, name, e = args.r, args.c, args.n, args.e
+
+    if not r and not c or r and c:
+        parser.print_help()
+        print("\nUsage error: you need to choose ONE of the required arguments.")
+        sys.exit(1)
+
+    print(64*"_", "\n")
+    if e:
+        print("resuming training..")
+
+    if r:
+        import config_regression as conf
+        #print(64*"_", "\n")
+        print("Analysis:   Regression")
+        print("Model name: {:}".format(name))
+        print(64*"_")
+
+
+    if c:
+        import config_classification as conf
+        #print(64*"_", "\n")
+        print("Analysis:   Classification")
+        print("Model name: {:}".format(name))
+        print(64*"_")
+
+    return conf, name, e
+
+def main(conf, name, e):
+    """
+    main script that trains the CNN etc.
+    """
+
+    X_train, X_test, y_train, y_test = conf.X_train, conf.X_test, conf.y_train, conf.y_test
+
+    if e:
+        history = continue_training(X_train, y_train, X_test, y_test, name, save_as=name, verbose=1)
+    else:
+        model = create_model()
+        history = train_model(X_train, y_train, X_test, y_test,
+                        model=model, verbose=1, save_as=name)
+
+
+    if conf.type == "classification":
+        loss, acc = model.evaluate(X_test, y_test, verbose=0)
+        print(65*"_", "accuracy: {:5.2f}%".format(100 * acc), 65*"_", sep="\n")
+
+
+    if conf.type == "regression":
+        loss, r2 = model.evaluate(X_test, y_test, verbose=0)
+        print(65*"_", "R2: {:5.2f}%".format(100*r2), 65*"_", sep="\n")
+
+
 
 
 
 if __name__ == '__main__':
-    create_model()
+    conf, name, e = arguments()
+    main(conf, name, e)
