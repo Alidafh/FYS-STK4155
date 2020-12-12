0d92b38697e7eb264aed81e6a9c92461ece6f2b2
alidafh <alidafh@gmail.com>
2020-12-12 15:10:05 +0100

    gerts code thing

--- project3/code/realistic_galaxy.py
+++ project3/code/realistic_galaxy.py
@@ -17,9 +17,9 @@ section_width = 10
 section_height = 10
 
 
-data_dge = fits.open("IEM_base_v2.fits")
-data_j = fits.open("j-factor-map-image.fits")
-data_gc = fitsio.read("map_model2_GC.fits")
+data_dge = fits.open("fits/IEM_base_v2.fits")
+data_j = fits.open("fits/j-factor-map-image.fits")
+data_gc = fitsio.read("fits/map_model2_GC.fits")
 
 
 map_dge = (data_dge[0].data[0,:,:]/np.max(data_dge[0].data[0,:,:])).clip(min=0)
@@ -30,8 +30,8 @@ map_gc = (data_gc/np.max(data_gc)).clip(min=0)
 # for i, row in enumerate(map_j):
 #     for j, el in enumerate(row):
 #         map_j[i,j] = max(0, map_j[i,j])
-        
-        
+
+
 
 dpp_dge = 0.125
 dpp_j = 0.05725971370143149
@@ -49,40 +49,40 @@ class dge():
         self.scale = scale
         self.dpp_x = section_width/self.dim[1]
         self.dpp_y = section_height/self.dim[0]
-        
+
     def func(self,y):
-        
+
         # print(E)
         row = np.zeros(self.dim[1])
         distance_y = y*self.dpp_y
-        
+
         for i, el in enumerate(row):
-            
+
             distance_x = (i - (self.dim[1]+1)/2 ) * self.dpp_x
             row[i] = map_dge[int(np.round(center_dge_y - distance_y/dpp_dge)), int(np.round(center_dge_x + distance_x/dpp_dge))]
-        
+
         return row*self.scale
-    
+
 # class dge():
 #     def __init__(self, dim = (28,28,10), scale=1):
 #         self.dim = dim
 #         self.scale = scale
 #         self.dpp_x = section_width/self.dim[1]
 #         self.dpp_y = section_height/self.dim[0]
-        
+
 #     def func(self,y, E):
-        
+
 #         # print(E)
 #         row = np.zeros(self.dim[1])
 #         distance_y = y*self.dpp_y
-        
+
 #         for i, el in enumerate(row):
-            
+
 #             distance_x = (i - (self.dim[1]+1)/2 ) * self.dpp_x
 #             row[i] = map_dge[E, int(np.round(center_dge_y - distance_y/dpp_dge)), int(np.round(center_dge_x + distance_x/dpp_dge))]
-        
-#         return row*self.scale    
-    
+
+#         return row*self.scale
+
 class dark_matter_profile():
     def __init__(self, dim = (28,28,10), scale=1):
         self.dim = dim
@@ -95,10 +95,10 @@ class dark_matter_profile():
         distance_y = (self.middle_row-i)*self.dpp_y
         distance_x = (self.middle_col-j)*self.dpp_x
         j = map_j[int(np.round(center_j - distance_y/dpp_j)), int(np.round(center_j+distance_x/dpp_j))]
-        
+
         return j*self.scale
-        
-    
+
+
 
 class galactic_center_spectrum():
     def __init__(self, scale=1, k0 = 1, E0=1, Ecut=1, gamma=1):
@@ -108,10 +108,10 @@ class galactic_center_spectrum():
         self.gamma=gamma
     def func(self,E):
         return self.k0 * ( E/self.E0 )**self.gamma * np.exp( -E/self.Ecut )
-    
-    
-    
-    
+
+
+
+
 class galactic_center_profile():
     def __init__(self, dim = (28,28,20), scale=1):
         self.dim = dim
@@ -124,13 +124,4 @@ class galactic_center_profile():
         distance_y = (self.middle_row-i)*self.dpp_y
         distance_x = (self.middle_col-j)*self.dpp_x
         flux = map_gc[int(np.round(center_gc - distance_y/dpp_gc)), int(np.round(center_gc+distance_x/dpp_gc))]
-        return flux*self.scale    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        return flux*self.scale
