3a0fefa92efbb40568b90ce1ce4eea84675f74bc
alidafh <alidafh@gmail.com>
2020-12-14 14:28:47 +0100

    new file

--- project3/code/config_regression.py
+++ project3/code/config_regression.py
@@ -18,15 +18,16 @@ from tools import preprocess, r2_score
 type = "regression"
 
 path = "../data/"
-filename = "maps_(1500, 28, 28, 20)_100.0_True_.npy"
+filename = "maps_(10000, 28, 28, 20)_0.008_0.0_0.0_2.0_1.0e+00_True_.npy"
 data_file = path+filename
 slice = None
 
 maps, labels, stats = load_data(file=data_file, slice=slice)
+
 (X_train, y_train), (X_test, y_test) = preprocess(maps, labels,
                                                 train_size = 0.8,
                                                 regress=True,
-                                                scale=True,
+                                                #scale=True,
                                                 seed=42,
                                                 shuffle=True)
 
@@ -37,11 +38,11 @@ maps, labels, stats = load_data(file=data_file, slice=slice)
 input_shape = (28, 28, 20)  # Shape of the images, holds the raw pixel values
 
 n_filters = 16              # For the first Conv2D layer
-kernel_size = (5,5)
-layer_config = [32, 64]         # (layer1, layer2, layer3, ....)
+kernel_size = (3,3)
+layer_config = None # [32, 64]         # (layer1, layer2, layer3, ....)
 
 connected_neurons = 128      # For the first Dense layer
-n_categories = 1            # For the last Dense layer
+n_categories = 1             # For the last Dense layer
 
 input_activation  = "relu"
 hidden_activation = "relu"
@@ -55,12 +56,14 @@ reg = None  #tf.keras.regularizers.l2(l=0.1)
 
 model_dir = "tmp/"           # Where to save the model
 
-epochs = 100
-batch_size = 10
+#epochs = 100
+epochs = 50
+batch_size = 20
 
-opt = tf.keras.optimizers.Adam(learning_rate=1e-5)
+opt = tf.keras.optimizers.Adam(learning_rate=1e-2)
+#opt = tf.keras.optimizers.SGD(learning_rate=1e-2)
 
-early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=30)
 
 loss = "mean_squared_error"
 metrics = [r2_score]
